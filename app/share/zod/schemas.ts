8605dc01cc4ef783535f7cfd8287f993184bea97
TABLET-6FSNT7EB\gojin <larc.ituki@gmail.com>
2025-03-15 18:36:43 +0900

    必須のバリデーションがかかっていなかったので修正

--- app/share/zod/schemas.ts
+++ app/share/zod/schemas.ts
@@ -2,13 +2,17 @@ import { z } from "zod";
 
 export const scrapyRequestSchema = z.object({
   // クロールするURL(文字列の配列)
-  startUrl: z.string({
-    message: "startUrl は文字列で指定してください。",
-  }),
+  startUrl: z
+    .string({
+      message: "startUrl は文字列で指定してください。",
+    })
+    .min(1, { message: "startUrl は必須項目です。" }),
   // クロール対象のクラス名
-  targetClass: z.string({
-    message: "targetClass は文字列で指定してください。",
-  }),
+  targetClass: z
+    .string({
+      message: "targetClass は文字列で指定してください。",
+    })
+    .min(1, { message: "targetClass は必須項目です。" }),
 });
 
 export type ScrapyRequest = z.infer<typeof scrapyRequestSchema>;
