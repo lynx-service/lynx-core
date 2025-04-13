4d4ba8ce38e178bf3ae2be9052629c1e7437ccb0
TABLET-6FSNT7EB\gojin <larc.ituki@gmail.com>
2025-04-13 18:53:26 +0900

    受け付けるデータサイズを調整

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express'; // expressからjsonとurlencodedをインポート
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // ペイロードサイズ制限を設定 (例: 50mb)
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+
   app.enableCors({
     origin: 'http://localhost:5173',
     credentials: true,
